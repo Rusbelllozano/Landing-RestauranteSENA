52eb09479d4a374fbef84b22603931538ca87f03
rusbelllozano <kiloloco2@gmail.com>
2019-12-08 18:30:14 -0500

    changes auth

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,13 +1,11 @@
-import cookieparser from 'cookieparser';
+
 export default function({store, route, redirect}) {
-    const parsed = cookieparser.parse(req.headers.cookie)
-        const accessTokenCookie= parsed.access_token;
     const user = store.state.users.user;
     const blockedRoute1= /\/pedidos\/*/g;
     const blockedRoute2= /\/productos\/*/g;
     const blockedRoute3= /\/caja\/*/g;
     const blockedRoute4= /\/cocina\/*/g;
-    if(!accessTokenCookie || !user && (route.path.match(blockedRoute1) || route.path.match(blockedRoute2)|| route.path.match(blockedRoute3)|| route.path.match(blockedRoute4))){
+    if(!user && (route.path.match(blockedRoute1) || route.path.match(blockedRoute2)|| route.path.match(blockedRoute3)|| route.path.match(blockedRoute4))){
         redirect("/admin")
     }
 }
