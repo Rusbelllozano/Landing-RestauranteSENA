81be1d4ec4aa1441996367d4adecc296526bb945
rusbelllozano <kiloloco2@gmail.com>
2019-12-09 15:46:56 -0500

    Changes in auth

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -5,7 +5,7 @@ export default function({store, route, redirect}) {
     const blockedRoute2= /\/productos\/*/g;
     const blockedRoute3= /\/caja\/*/g;
     const blockedRoute4= /\/cocina\/*/g;
-    if(!user || (route.path.match(blockedRoute1) || route.path.match(blockedRoute2)|| route.path.match(blockedRoute3)|| route.path.match(blockedRoute4))){
+    if(!user && (route.path.match(blockedRoute1) || route.path.match(blockedRoute2)|| route.path.match(blockedRoute3)|| route.path.match(blockedRoute4))){
         redirect("/admin")
     }
 }
